f2146ac429d35844aef6099d16245f7e1aa2f3db
fordpref <mike@ermanfamily.com>
2013-12-02 21:11:26 -0500

    Number of handlers added, tested on win7 and XP
    Think everything XP and newer should work...probably not windows 8.
    Also used Pyinstaller to create an executable.  Needs a number of python
    modules and that list is likely to expand.

--- collection-script.py
+++ collection-script.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import sys
-import os
+import sys, os, time, platform, socket, _winreg, subprocess, win32security
 from datetime import datetime
-import time
-import platform
-import socket
-import _winreg
+from subprocess import PIPE
+from win32netcon import *
+from psutil import *
+
+
 
 
 """
@@ -19,6 +19,7 @@ hostname = ""
 ipaddr = ""
 aname = ""
 dir = ""
+sqldir = ""
 reg = ""
 registry = {}
 
@@ -109,21 +110,24 @@ def get_ip():
                 print "Incorrect, pick one of the above!"
             else:
                 return listip[int(selection)]
+    return listip[0]
 
 def make_assessment_name():
     """
     Here we combine a few of the above to create a descriptive directory and filename structure.
     This ensures that later we know what we collected and validate it is complete.
     """
-    global date, hostname, ipaddr, aname, dir
+    global date, hostname, ipaddr, aname, dir, sqldir
     import os
     
     dir = "c:\\assessment\\"
     if os.path.exists(dir) == False:
         os.mkdir(dir)
-    aname = date + "-" + hostname + "-" + ipaddr
-    dir = dir + aname
+    aname = str(date) + "-" + hostname + "-" + str(ipaddr)
+    dir = dir + aname + "\\"
+    sqldir = dir + "sql\\"
     os.mkdir(dir)
+    os.mkdir(sqldir)
     return
 
 def sysint_registry(action):
@@ -164,6 +168,218 @@ def sysint_registry(action):
             else:
                 _winreg.DeleteKey(_winreg.HKEY_CURRENT_USER, key)
 
+def collection():
+    global path, date, sys_os, hostname, ipaddr, aname, dir, sqldir
+    infile = []
+    outfile = ""
+    sqlfile = ""
+    sqlline = ""
+    info = []
+    flag = 0
+    flag2 = 0
+    flag3 = 0
+    proc = 0
+    processor = ""
+    commandline = ""
+    bname = dir + aname
+    sqlname = sqldir + aname
+    
+    
+    
+    infile = subprocess.Popen("systeminfo.exe", stdout=PIPE, stderr=PIPE)
+    infile = infile.communicate()[0].split("\n")
+    outfile = open(bname + "-systeminfo.txt", 'w')
+    sqlfile = open(sqlname + "-systeminfo.csv", 'w')
+    sqlfile.write("IP")
+    for line in infile:
+        line = line[:-1]
+        if line != "" and line.startswith("Hotfix") == 0 and flag == 0:
+            info = line.split(":", 1)
+            info[0] = info[0].lstrip()
+            info[1] = info[1].lstrip()
+            if info[0].startswith("Proc") == 1:
+                processor = info[0]
+                proc = 1
+            elif proc == 1:
+                info[0] = processor
+                outfile.write(info[0] + "," + info[1] + "\n")
+                sqlfile.write("," + info[0])
+                sqlline += "," + info[1]
+                proc = 0
+            else:
+                info[1] = info[1].split(",")[0]
+                outfile.write(info[0] + "," + info[1] + "\n")
+                sqlfile.write("," + info[0])
+                sqlline += "," + info[1]
+        elif line.startswith("Hotfix") == 1:
+            flag = 1
+            outfile.close
+            sqlfile.write("\n" + ipaddr + sqlline)
+            sqlfile.close
+    
+    #Get the Software Registry        
+    infile = subprocess.Popen("regedit.exe /E /S " + bname + "-Software-Registry.reg HKEY_LOCAL_MACHINE\\Software")
+    
+    #get the environment variables CSV and raw file
+    outfile = open(sqlname + "-environment-variables.csv", 'w')
+    rawfile = open(bname + "-env-variables.txt", 'w')
+    header = "key/ip,VarName,Value\n"
+    outfile.write(header)
+    for key in os.environ:
+        outfile.write(ipaddr + "," + key + "," + os.environ[key] + "\n")
+        rawfile.write(key + ":\t" + os.environ[key] + "\n")
+    rawfile.close
+    outfile.close
+    
+    
+    #get IP and Interface information
+    with open(bname + '-ipconfig.txt', 'w') as outfile:
+        subprocess.call('ipconfig.exe /all', stdout=outfile)
+    
+    #get AD DC and domain name information
+    dc = win32security.DsGetDcName()
+    if dc:
+        dcfile = open(sqlname + '-wkstation-AD-info.csv', 'w')
+        header = "IP,DomainControllerAddress,DnsForestName,DomainName,DomainControllerName"
+        dcfile.write(header + "\n")
+        dcfile.write(ipaddr + "," + dc['DomainControllerAddress'][2:] + "," + 
+                     dc['DnsForestName'] + "," + dc['DomainName'] + "," + dc['DomainControllerName'][2:]
+                     + "\n")
+        dcfile.close
+    
+    #get IP routing informatino
+    with open(bname + '-Routes.txt', 'w') as outfile:
+        subprocess.call('netstat.exe -nr ', stdout=outfile)
+       
+    #insert IP ROUTING csv table here
+    infile = open(bname + '-Routes.txt', 'r')
+    for line in infile:
+        if line.startswith('Network Destination'):
+            headers = line.split()
+            headers[0] = 'key/ip'
+            headers = ",".join(headers)
+            flag = 1
+            outfile = open(sqlname + '-ipv4-route-table.csv', 'w')
+            outfile.write(headers + '\n')
+        elif line.startswith('Persistent') and flag2 == 0:
+            flag = 2
+            flag2 = 1
+        elif line.startswith('Persistent') and flag2 == 1:
+            flag = 5
+            flag2 = 0
+        elif line.startswith('Active'):
+            flag = 4
+        elif line.startswith('=') and flag == 1:
+            flag = 0
+            outfile.close
+        elif flag == 1:
+            route = line.split()
+            if flag3 == 1 and len(route) == 3:
+                route.append('On-link')
+                flag3 = 2
+            if flag3 == 2 and len(route) == 1:
+                flag3 = 3
+            
+            if flag == 1 and (flag3 == 0 or flag3 == 1 or flag3 == 2):                
+                route = ",".join(route)
+                outfile.write(ipaddr + ',' + route + "\n")
+            elif flag3 == 3:
+                flag3 = 1
+        elif flag == 2:
+            headers = line.split()
+            if len(headers) == 1:
+                headers[0] = 'key/ip'
+                headers.append('Destination\n')
+                headers = ",".join(headers)
+                outfile = open(sqlname + '-ipv4-persistent-routes.csv', 'w')
+                outfile.write(headers)
+                outfile.write(ipaddr + "," + "None")
+                flag = 0
+            else:
+                headers[0] = 'key/IP'
+                headers[1] = 'Destination'
+                headers.pop(4)
+                headers = ",".join(headers)
+                outfile = open(sqlname + '-ipv4-persistent-routes.csv', 'w')
+                outfile.write(headers + '\n')
+                flag = 1
+        elif flag == 4:
+            headers = line.split()
+            headers.insert(0, 'key/ip')
+            headers.pop(3)
+            headers = ",".join(headers)
+            outfile = open(sqlname + '-ipv6-routes.csv', 'w')
+            outfile.write(headers + '\n')
+            flag = 1
+            flag3 = 1
+        elif flag == 5:
+            outfile = open(sqlname + '-ipv6-persisten-routes.csv', 'w')
+            outfile.write(line)
+            outfile.write('\nIf there are IPV6 persistent routes, capture and re-write this module')
+
+    
+    #Get Network Listeners Raw
+    with open(bname + '-TCP-NetworkListeners.txt', 'w') as outfile:
+        p1 = subprocess.Popen('netstat.exe -nao', stdout=PIPE)
+        p2 = subprocess.Popen('findstr /c:LISTEN', stdin=p1.stdout, stdout=outfile)
+    with open(bname + '-UDP-NetworkListeners.txt', 'w') as outfile:
+        p1 = subprocess.Popen('netstat.exe -nao', stdout=PIPE)
+        p2 = subprocess.Popen('findstr /c:*:*', stdin=p1.stdout, stdout=outfile)
+        
+    
+    #Get Network Listeners CSV
+    """
+    Uses a combination of PSUTIL module and Sysinternals and OS utilities
+    to create two tables for TCP and UDP and give src, dst, ports, PIDs,
+    and command line or executable
+    """
+    
+    netstat_header = []
+    tcp_listen = open(sqlname + '-netstat-listeners-TCP.csv', 'w')
+    udp_listen = open(sqlname + '-netstat-listeners-UDP.csv', 'w')
+    
+    netstat = subprocess.Popen('netstat.exe -nao', stdout=PIPE, stderr=PIPE)
+    netstat = netstat.communicate()[0].split("\n")
+    for line in netstat:
+        line = line.split()
+        
+        if line and line[0] == "Proto":
+            line.pop(2)
+            line.pop(3)
+            line.append("Executable")
+            line.insert(0,"key/ip")
+            tcp_listen.write(",".join(line) + '\n')
+            line.pop(4)
+            udp_listen.write(",".join(line) + '\n')
+        elif line and line[0] == "TCP" and line[3] == "LISTENING":
+            process = Process(int(line[4]))
+            try:
+                cmd = process.exe
+                line.append(cmd)
+            except:
+                process = subprocess.Popen([path + 'tcpvcon.exe', '-a', '-c', '-n', line[4]], stdout=PIPE)
+                process = process.communicate()[0].split(",")
+                line.append(process[1])
+            line.insert(0, ipaddr)
+            tcp_listen.write(",".join(line) + '\n')
+        elif line and line[0] == "UDP" and line[2] == "*:*":
+            process = Process(int(line[3]))
+            try:
+                cmd = process.exe
+                line.append(cmd)
+            except:
+                process = subprocess.Popen([path + 'tcpvcon.exe', '-a', '-c', '-n', line[3]], stdout=PIPE)
+                process = process.communicate()[0].split(",")
+                line.append(process[1])
+            line.insert(0, ipaddr)
+            udp_listen.write(",".join(line) + '\n')
+    tcp_listen.close
+    udp_listen.close
+
+    
+    
+    
+
 #print out usage message
 print "\nWelcome to the Vulnerability Assessment Collection Tool (VZP VAC).\n\n"
 print "This script is designed to run locally on a system where"
@@ -197,6 +413,10 @@ make_assessment_name()
 
 #create the sysinternals EULA registry keys
 sysint_registry('create')
+
+#Start first part of collection
+collection()
+
 
 
 
